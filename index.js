4412090219e6435152bae5d63b5e47cd4d17d3a3
Dan Kibaara <kibaara.gamedev@gmail.com>
2024-10-15 09:49:26 +0300

    Adds  Arithmetic expressions

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//multiply
+let num1=1;
+let num2=62;
+let multiply= (num1 * num2);
+
+//random
+let random = Math.round(Math.random()*100 +1)
+
+
+//mod
+let num3=52;
+let num4=12;
+let mod=(num3 % num4);
+
+//max
+let max= Math.max(1,4,20)
